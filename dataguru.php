031a0233f44a91f626604135c90746359c7eab73
Rakentod <141595556+Rakentod@users.noreply.github.com>
2023-08-21 08:41:48 +0700

    Create dataguru.php
    yoooooooooooooooooooooooooooooooooooooooooooooooooooooooo

---
+++ dataguru.php
@@ -0,0 +1,10 @@
+Disini adalah hhalaman untuk menampilkan data guru dengan menggunakan table
+teble data guru terdiri dari
+-ID
+-Nama
+-Jenis kelamin
+-Alamat
+-Mata pelajaran
+-Action:
+-Edit
+-Delete
